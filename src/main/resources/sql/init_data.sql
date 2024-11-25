54a8386a50d8656ce4bbdaaaf504f908d8c47a70
lalalalz <lalalalz@naver.com>
2024-11-25 23:07:07 +0900

    feature-001 - 초기화 데이터 SQL 추가

---
+++ src/main/resources/sql/init_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO exchange.exchange_rates (id, country, country_flag, country_code, currency_code, rate_value, updated_at)
+VALUES (1, 'korea', '🇰🇷', 'KR', 'KRW', 1, '2024-11-25 12:28:29');
+INSERT INTO exchange.exchange_rates (id, country, country_flag, country_code, currency_code, rate_value, updated_at)
+VALUES (5, 'usa', '🇺🇸', 'US', 'USD', 1377.68, '2024-11-24 21:54:23');
+INSERT INTO exchange.exchange_rates (id, country, country_flag, country_code, currency_code, rate_value, updated_at)
+VALUES (6, 'Japanese', '🇯🇵', 'JP', 'JPY', 886.91, '2024-11-24 21:54:23');
+INSERT INTO exchange.exchange_rates (id, country, country_flag, country_code, currency_code, rate_value, updated_at)
+VALUES (7, 'Chinese', '🇨🇳', 'CN', 'CNH', 190.35, '2024-11-25 13:57:58');
+INSERT INTO exchange.exchange_rates (id, country, country_flag, country_code, currency_code, rate_value, updated_at)
+VALUES (9, 'Australia', '🇦🇺', 'AU', 'AUD', 896.59, '2024-11-24 21:54:23');
