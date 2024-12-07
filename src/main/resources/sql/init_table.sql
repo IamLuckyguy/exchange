24b8d7b9558ceafd969bb286ab6511a1d3d4cee1
lucky <lucky@wemakeprice.com>
2024-12-07 22:22:26 +0900

    - 로컬 DB 접속 정보 변경 - dev DB - front 기능 개선   - /api/exchange-rates 에서 데이터를 받아오기 전 Loading 표시   - 국가 선택 체크박스 제거, 버튼으로 변경   - 선택된 통화, 차트에 표시할 통화, 기간 선택 시 로컬스토리지에 저장, 이후 접속에도 저장된 설정으로 표기되도록 개선

--- src/main/resources/sql/init_table.sql
+++ src/main/resources/sql/init_table.sql
@@ -1,32 +1,30 @@
-CREATE TABLE IF NOT EXISTS exchange_rates
-(
-    id            BIGINT AUTO_INCREMENT PRIMARY KEY,                                          -- 기본 키
-    currency_code VARCHAR(10) NOT NULL,                                                       -- 통화 코드
-    unit_amount   INT                  DEFAULT 1,
-    decimals      INT                  DEFAULT 1,
-    rate_value    DOUBLE      NOT NULL,                                                       -- 환율 값
-    updated_at    DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, -- 마지막 업데이트 시간
-    INDEX idx_currency_code (currency_code)                                                   -- 통화 코드 인덱스
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `exchange_rates` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `currency_code` VARCHAR(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `unit_amount` INT DEFAULT '1',
+    `decimals` INT DEFAULT '1',
+    `rate_value` DOUBLE NOT NULL,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    KEY `idx_currency_code` (`currency_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE exchange_rate_histories
-(
-    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    currency_code VARCHAR(10) NOT NULL,
-    rate_value    DOUBLE      NOT NULL,
-    updated_at    DATETIME    NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `exchange_rate_histories` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `currency_code` VARCHAR(10) COLLATE utf8mb4_general_ci NOT NULL,
+    `rate_value` DOUBLE NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE country
-(
-    id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    currency_code VARCHAR(10) UNIQUE  NOT NULL,
-    country_name  VARCHAR(100) UNIQUE NOT NULL,
-    country_flag  VARCHAR(255)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS `country` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `currency_code` VARCHAR(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `country_name` VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `country_flag` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `currency_code` (`currency_code`),
+    UNIQUE KEY `country_name` (`country_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
