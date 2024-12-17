0bbc9a321bde1c80543a278296e452192ffb58c1
lalalalz <lalalalz@naver.com>
2024-12-17 23:56:14 +0900

    feature/kwt-011 > fetch API End Point 수정 - 기존 바디로 요청받은 패치 요청 날짜를 파라미터로 수정 - 파라미터는 필수값이 아니고 오늘 날짜로 기본값 처리

--- src/main/java/kr/co/kwt/exchange/adapter/in/api/controller/ExchangeRateApiController.java
+++ src/main/java/kr/co/kwt/exchange/adapter/in/api/controller/ExchangeRateApiController.java
@@ -1,6 +1,5 @@
 package kr.co.kwt.exchange.adapter.in.api.controller;
 
-import kr.co.kwt.exchange.adapter.in.api.dto.FetchExchangeRateRequest;
 import kr.co.kwt.exchange.adapter.in.api.dto.FetchExchangeRateResponse;
 import kr.co.kwt.exchange.adapter.in.api.service.ExchangeRateApiService;
 import kr.co.kwt.exchange.application.port.in.dto.AddExchangeRateRequest;
@@ -9,9 +8,12 @@ import kr.co.kwt.exchange.application.port.in.dto.GetExchangeRateRequest;
 import kr.co.kwt.exchange.application.port.in.dto.GetExchangeRateResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
 
 @Slf4j
 @RestController
@@ -42,9 +44,13 @@ public class ExchangeRateApiController {
 
     @PostMapping(value = "/exchange-rates/fetch")
     public Flux<FetchExchangeRateResponse> fetchExchangeRates(
-            @RequestBody final FetchExchangeRateRequest fetchExchangeRateRequest
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @RequestParam(
+                    value = "fetchDate", required = false,
+                    defaultValue = "#{T(java.time.LocalDate).now().toString()}"
+            ) final LocalDate fetchDate
     ) {
-        log.info("fetchExchangeRates 호출 성공 : {}", fetchExchangeRateRequest.getFetchDateTime());
-        return exchangeRateApiService.fetchExchangeRates(fetchExchangeRateRequest);
+        log.info("fetchExchangeRates 호출 성공 : {}", fetchDate);
+        return exchangeRateApiService.fetchExchangeRates(fetchDate);
     }
 }
